478cce043bc281c738bd3f5266d1b57b68f12c3e
BrandonKim <brandonkim@virginia.edu>
2020-05-29 01:28:37 -0400

    Created Map class and created instance methods

--- W3D5/diy_atds.rb
+++ W3D5/diy_atds.rb
@@ -38,5 +38,35 @@
     end
   end
 
-  
+  #Exercise 3
+
+  class Map
+    def initialize
+      @array = []
+    end
+
+    def set(key, value)
+        index_of_pair = @array.index { |pair| pair.first == key }
+        if index_of_pair
+            @array[index_of_pair][1] = value #why didn't .last work?
+        else 
+            @array.push([key, value])
+        end
+    end
+
+    def get(key)
+      @array.each { |pair| return pair.last if pair.first == key }
+
+    end
+
+    def delete(key)
+      index_of_pair = @array.index { |pair| pair.first == key }
+      @array.delete_at(index_of_pair)
+    end
+
+    def show
+      return @array
+    end
+  end
+
 
