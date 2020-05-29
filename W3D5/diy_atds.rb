93809d064ae1137e1749ce5537c9ac878180c1ea
BrandonKim <brandonkim@virginia.edu>
2020-05-29 00:24:16 -0400

    Created Queue class and instance methods

--- W3D5/diy_atds.rb
+++ W3D5/diy_atds.rb
@@ -18,4 +18,25 @@
     end
   end
 
+  #Exercise 2
+
+  class Queue
+    def initialize
+      @array = []
+    end
+
+    def enqueue(el)
+      @array.unshift(el)
+    end
+
+    def dequeue
+      @array.pop
+    end
+
+    def peek
+      @array[-1]
+    end
+  end
+
   
+
