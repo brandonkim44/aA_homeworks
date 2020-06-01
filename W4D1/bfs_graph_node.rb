b7f823e6dc5aae8363955841d9cf0a9cc77b0309
BrandonKim <brandonkim@virginia.edu>
2020-05-31 20:57:06 -0400

    Created GraphNode Class and BFS method

--- W4D1/bfs_graph_node.rb
+++ W4D1/bfs_graph_node.rb
@@ -1,3 +1,36 @@
+require 'set'
 class GraphNode
-    
+    attr_accessor :value, :neighbors
+    def initialize(value)
+        self.value = value
+        self.neighbors = []
+    end
 end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+
+def bfs(starting_node, target_value)
+    visited = Set.new()
+    queue = [starting_node]
+    until queue.empty?
+        node = queue.shift
+        if visited.include?(node) != false
+            return node.value if node.value == target_value
+        else
+            visited.add(node)
+            queue += starting_node.neighbors
+        end
+    end
+    nil
+end
+
+p bfs(f, 'x')
