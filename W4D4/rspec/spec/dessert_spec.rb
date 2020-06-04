873ce3160e890f451f878cafa1f00aa3efd0cf75
BrandonKim <brandonkim@virginia.edu>
2020-06-04 01:48:27 -0400

    Wrote it blocks for dessert_spec_rb

--- W4D4/rspec/spec/dessert_spec.rb
+++ W4D4/rspec/spec/dessert_spec.rb
@@ -7,31 +7,52 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 
 describe Dessert do
   let(:chef) { double("chef") }
+  let (:brownie) { Dessert.new("brownie", 50, chef) }
 
   describe "#initialize" do
     it "sets a type" do
-      expect(chef.type).to eq("chef")
+      expect(brownie.type).to eq("brownie")
     end 
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(brownie.quantity).to eq(50)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(brownie.ingredients).to be_empty
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("brownie", "50", chef) }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    before(:each) { brownie.ingredients << "flour" }
+    it "adds an ingredient to the ingredients array" do
+      expect(brownie.ingredients).to include("flour")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      brownie.ingredients = ["flour", "chocolate", "eggs", "milk", "butter"]
+      shuffled = brownie.ingredients.shuffle
+      val = huffled.all? { |ingredient| shuffled.include?(ingredient) }
+      expect(val).to be true
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      brownie.eat(10)
+      expect(brownie.quantity).to eq(40)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      brownie.eat(50)
+      expect { brownie.quantity <= 0 }.to raise_error("the amount is too great")
+    end
   end
 
   describe "#serve" do
