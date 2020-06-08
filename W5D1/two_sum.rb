f11820aef8a07b98a38366d95ac56ec8e065729d
BrandonKim <brandonkim@virginia.edu>
2020-06-08 09:20:13 -0400

    Finished writing bad_two_sum? method

--- W5D1/two_sum.rb
+++ W5D1/two_sum.rb
@@ -1,13 +1,6 @@
-two_sum?
+# two_sum?
 # Given an array of unique integers and a target sum, determine whether any two integers in the array sum to that amount.
 
-def two_sum?(arr, target_sum)
-    # your code here...
-end
-
-# arr = [0, 1, 5, 7]
-# two_sum?(arr, 6) # => should be true
-# two_sum?(arr, 10) # => should be false
 
 # If we weren't worried about time complexity, what would be the most straightforward way to solve this problem? 
 # To start with, we could check every possible pair. If we sum each element with every other, 
@@ -18,7 +11,18 @@ end
 
 # Let's start by implementing the brute force solution. Write a method called bad_two_sum?, which checks every possible pair.
 
+def bad_two_sum?(arr, target_sum)
+    arr.each_with_index do |ele1, idx1|
+        arr.each_with_index do |ele2, idx2|
+            return true if idx2 > idx1 && (ele1+ele2 == target_sum)
+        end
+    end
+    false
+end
 
+# arr = [0, 1, 5, 7]
+# p bad_two_sum?(arr, 6) # => should be true
+# p bad_two_sum?(arr, 10)
 
 # Sort your data, then try to solve the problem.
 
@@ -26,8 +30,25 @@ end
 # How might sorting that make the problem easier?
 # Write a second solution, called okay_two_sum?, which uses sorting. Make sure it works correctly.
 
+def okay_two_sum?(arr, target_sum)
+
+end
+
+arr = [0, 1, 5, 7]
+p okay_two_sum?(arr, 6) # => should be true
+p okay_two_sum?(arr, 10)
+
+
 # Finally, it's time to bust out the big guns: a hash map. Remember, a hash map has O(1) #set and O(1) #get, 
 # so you can build a hash out of each of the n elements in your array in O(n) time. 
 # That hash map prevents you from having to repeatedly find values in the array; now you can just look them up instantly.
 
 # See if you can solve the two_sum? problem in linear time now, using your hash map.
+
+def better_two_sum?(arr, target_sum)
+
+end
+
+arr = [0, 1, 5, 7]
+# p two_sum?(arr, 6) # => should be true
+# p two_sum?(arr, 10)
