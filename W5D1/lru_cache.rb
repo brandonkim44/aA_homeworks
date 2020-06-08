d65c9b0ad73f02f9ae5af34589698156cc25ec8f
BrandonKim <brandonkim@virginia.edu>
2020-06-08 01:54:56 -0400

    Filled out methods in skeleton

--- W5D1/lru_cache.rb
+++ W5D1/lru_cache.rb
@@ -9,8 +9,14 @@
     end
 
     def add(el)
-        
-
+        if self.count < @max_cache_size && @underlying_array.include?(el)
+            delete(el)
+            @underlying_array << el
+        elsif self.count < @max_cache_size
+            @underlying_array << el
+        else
+            rotate(el)
+        end
     end
 
     def show
@@ -19,25 +25,38 @@
 
     private
     # helper methods go here!
+    def rotate(el)
+        @underlying_array.shift
+        @underlying_array << el
+    end
 
+    def index(el)
+       index = @underlying_array.index(el) if @underlying_array.include?(el)
+       index
+    end
+
+    def delete(el)
+        index = index(el)
+        @underlying_array.delete_at(index)
+    end
   end
 
-#   johnny_cache = LRUCache.new(4)
+  johnny_cache = LRUCache.new(4)
 
-#   johnny_cache.add("I walk the line")
-#   johnny_cache.add(5)
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(5)
 
-#   johnny_cache.count # => returns 2
+  johnny_cache.count # => returns 2
 
-#   johnny_cache.add([1,2,3])
-#   johnny_cache.add(5)
-#   johnny_cache.add(-5)
-#   johnny_cache.add({a: 1, b: 2, c: 3})
-#   johnny_cache.add([1,2,3,4])
-#   johnny_cache.add("I walk the line")
-#   johnny_cache.add(:ring_of_fire)
-#   johnny_cache.add("I walk the line")
-#   johnny_cache.add({a: 1, b: 2, c: 3})
+  johnny_cache.add([1,2,3])
+  johnny_cache.add(5)
+  johnny_cache.add(-5)
+  johnny_cache.add({a: 1, b: 2, c: 3})
+  johnny_cache.add([1,2,3,4])
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(:ring_of_fire)
+  johnny_cache.add("I walk the line")
+  johnny_cache.add({a: 1, b: 2, c: 3})
 
 
-#   johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
+  p johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
