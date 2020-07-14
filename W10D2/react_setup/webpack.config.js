c457b8e779d705cad794670d6c5e50685bd4eb04
BrandonKim <brandonkim@virginia.edu>
2020-07-14 00:26:07 -0400

    set up first react app

---
+++ W10D2/react_setup/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    context: __dirname, //need this?
+    entry: './entry.jsx',
+    output: {
+        path: path.resolve(__dirname), //need this?
+        filename: './bundle.js'
+    }, 
+    module: { //need to memorize?
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+};
